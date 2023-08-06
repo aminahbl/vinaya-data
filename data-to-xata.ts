900b16cb24fc92948b3226bcb3e8e9b081674d96
aminahbl <aminahbl.github@37e.uk>
2023-08-06 20:31:35 +0200

    load prelim data to xata & add timeout

--- data-to-xata.ts
+++ data-to-xata.ts
@@ -3,27 +3,46 @@ import data from "./v0/json";
 
 const xata = getXataClient();
 
+// this is to limit the occurrences of requests stalling due to hitting Xata limits 
+const sleep = (fn: (record: any) => void, delay: number) => {
+  return (record: any, i: number) => {
+    setTimeout(() => {
+      fn(record);
+    }, i * delay);
+  }
+};
+
 const updateXata = () => {
-  data.languages.forEach(async (rule) => {
-    const { id, ...record } = rule;
+  const createOrUpdateLanguage = async (language: any) => {
+    const { id, ...record } = language;
     await xata.db.languages.createOrUpdate(id, record);
-  });
-  data.translators.forEach(async (rule) => {
-    const { id, ...record } = rule;
+  };
+  data.languages.forEach(sleep(createOrUpdateLanguage, 600));
+
+  const createOrUpdateTranslator = async (translator: any) => {
+    const { id, ...record } = translator;
     await xata.db.translators.createOrUpdate(id, record);
-  });
-  data.pliTvPmCategories.forEach(async (rule) => {
-    const { id, ...record } = rule;
+  };
+  data.translators.forEach(sleep(createOrUpdateTranslator, 600));
+
+  const createOrUpdateCategory = async (category: any) => {
+    const { id, ...record } = category;
     await xata.db.pli_tv_pm_categories.createOrUpdate(id, record);
-  });
-  data.pliTvPmBiRules.forEach(async (rule) => {
+  };
+  data.pliTvPmCategories.forEach(sleep(createOrUpdateCategory, 600));
+
+  const createOrUpdateRule = async (rule: any) => {
     const { id, ...record } = rule;
     await xata.db.pli_tv_pm_bi_rules.createOrUpdate(id, record);
-  });
-  data.pliTvPmBiTranslations.forEach(async (rule) => {
-    const { id, ...record } = rule;
+  };
+  data.pliTvPmBiRules.forEach(sleep(createOrUpdateRule, 600));
+
+  const createOrUpdateTranslation = async (translation: any) => {
+    const { id, ...record } = translation;
     await xata.db.pli_tv_pm_bi_translations.createOrUpdate(id, record);
-  });
+  };
+  data.pliTvPmBiTranslations.forEach(sleep(createOrUpdateTranslation, 600));
 };
+
 
 updateXata();
